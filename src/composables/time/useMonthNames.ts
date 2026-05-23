f88f371264c50ea8ebe0d653b1254211eedc0d7b
Simon Reinisch <simon@reinisch.io>
2026-05-23 14:05:57 +0200

    feat: use memoization in useMonthNames for improved performance
    significantly improves the performance of switching between income/expenses and other pages

--- src/composables/time/useMonthNames.ts
+++ src/composables/time/useMonthNames.ts
@@ -1,24 +1,27 @@
+import { useMemoize } from '@vueuse/core';
 import { computed, ComputedRef, MaybeRefOrGetter, toValue } from 'vue';
 import { useI18n } from 'vue-i18n';
 
-export const useMonthNames = (
-  format: Intl.DateTimeFormatOptions['month'] = 'long',
-  offset: MaybeRefOrGetter<number> = 0
-): ComputedRef<string[]> => {
-  const { locale } = useI18n();
+export const useMonthNames = useMemoize(
+  (
+    format: Intl.DateTimeFormatOptions['month'] = 'long',
+    offset: MaybeRefOrGetter<number> = 0
+  ): ComputedRef<string[]> => {
+    const { locale } = useI18n();
 
-  return computed(() => {
-    const resolvedOffset = toValue(offset);
-    const list = [];
+    return computed(() => {
+      const resolvedOffset = toValue(offset);
+      const list = [];
 
-    for (let i = 0; i < 12; i++) {
-      list.push(
-        new Date(0, (i + resolvedOffset) % 12).toLocaleDateString(locale.value, {
-          month: format
-        })
-      );
-    }
+      for (let i = 0; i < 12; i++) {
+        list.push(
+          new Date(0, (i + resolvedOffset) % 12).toLocaleDateString(locale.value, {
+            month: format
+          })
+        );
+      }
 
-    return list;
-  });
-};
+      return list;
+    });
+  }
+);
